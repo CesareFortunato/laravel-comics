20ac54ccc78ed02215d8a712cf846b4063c9118d
CesareFortunato <cesare.fortunato89@gmail.com>
2026-04-14 18:34:10 +0200

    feat: create pages + routes + layout

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>about</title>
 </head>
 <body>
     @extends('layouts.master')
 
     @section('content')
-    <h1>Home</h1>
+    <h1>about</h1>
     @endsection
 </body>
 </html>
